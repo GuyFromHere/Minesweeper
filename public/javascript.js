0a05233b3af65935d274dc2618a18d1ed2724578
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2019-05-27 07:19:11 -0700

    It works! TODO: Add bells and whistles.

--- public/javascript.js
+++ public/javascript.js
@@ -1,10 +1,13 @@
+// Create new empty dictionary to store cell objects.
+var cellDictionary = {};
+
+
 function getBoard(difficulty) {
     // Add button to reset page with desired difficulty. 
     // Load easy by default.
     
     // Variables
     let cellIndex = [ 4, 10, 16 ];
-    let rowIndex = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
     var min = 1;
     var max = 10;
 
@@ -27,64 +30,79 @@ function getBoard(difficulty) {
         }
     }
     
+    // grid boundaries (in relation to clicked cell) when checking for mines
+    const surroundingGrid = [ -(cellIndex[difficulty]-1), -(cellIndex[difficulty]), -(cellIndex[difficulty]+1), -1, 1, (cellIndex[difficulty]-1), (cellIndex[difficulty]), (cellIndex[difficulty]+1) ];
+
     // build board with area cellCount x cellCount
-    for (var i = 0; i<cellIndex[difficulty]; i++) {  
-        for (var x = 1; x<=cellIndex[difficulty]; x++) {
-            // get random number to determine if mine or safe
-            var random = Math.floor(Math.random() * (+max - +min)) + +min
-
-            // create cell divs
-            const cellId = (rowIndex[i] + String(x));
-            var newNode = document.createElement('div')
-            newNode.id = rowIndex[i] + x
-            newNode.className = 'cell';
-            newNode.setAttribute('oncontextmenu',"javascript:flag(this.id);return false;");
-            newNode.count = i;
-            var newCell = {
-                id: newNode.id,
-                isMine: false,
-                isFlagged: false
-            };
-
-            // define click behavior.
-            if ( random > 5 ) {
-                newNode.onclick = function() {
-                    var element = document.getElementById(this.id);
-                    element.className = 'boom';
-                };
-                newCell.isMine = true;
-            } else {
-                newNode.onclick = function() {
-                    var element = document.getElementById(this.id);
-                    element.className = 'safe';
-                };
-            }
-                       
-            newNode.innerHTML = cellId;
-            board.appendChild( newNode );
-        }
+    for (var i = 0; i<(cellIndex[difficulty]*cellIndex[difficulty]); i++) {  
+        // get random number to determine if mine or safe
+        var random = Math.floor(Math.random() * (+max - +min)) + +min
+        // create cell divs
+       const cellId = i;
+       
+        var newNode = document.createElement('div')
+        newNode.id = i;
+        newNode.className = 'cell';
+        newNode.setAttribute('oncontextmenu',"javascript:flag(this.id);return false;");
+        newNode.count = i;
+        var newCell = {
+            id: newNode.id,
+            isMine: false
+        };
         
+        // define click behavior.
+        if ( random > 5 ) {
+            newCell.isMine = true;
+            newNode.onclick = function() {
+                var element = document.getElementById(this.id);
+                element.className = 'boom'; 
+            };
+        } else {
+            newNode.onclick =  function() {
+                let counter = 0;
+                for (var cells of surroundingGrid) {
+                    let it = Number(this.id) + Number(cells);
+                    if ( it < 0 || it >= (cellIndex[difficulty]*cellIndex[difficulty]) ) {
+                        // ignore grid if it goes out of bounds
+                       // do nothing
+                    } else if ( Number(this.id) % cellIndex[difficulty] == 0) {
+                        // First column. Skip cells to the left.
+                        if ( it == ((Number(this.id)-cellIndex[difficulty])-1) ||
+                             it == (Number(this.id) - 1) ||
+                             it == ((Number(this.id)+cellIndex[difficulty])-1)) { 
+                                // do nothing
+                        } else if (cellDictionary[it].isMine) {
+                            counter += 1;
+                        }
+                    } else if ( (Number(this.id) - (cellIndex[difficulty]-1)) % cellIndex[difficulty] == 0) {
+                        // Last column. Skip cells to the right.
+                        if ( it == ((Number(this.id)-cellIndex[difficulty])+1) ||
+                             it == (Number(this.id) + 1) ||
+                             it == ((Number(this.id)+cellIndex[difficulty])+1)) { 
+                                // do nothing
+                        } else if (cellDictionary[it].isMine) {
+                            counter += 1;
+                        }
+                    } else if (cellDictionary[it].isMine) {
+                        counter += 1;
+                    }
+                }
+                var element = document.getElementById(this.id);
+                element.className = 'safe'; 
+                element.innerHTML = counter;
+            };
+        }    
+        //newNode.innerHTML = cellId;
+        cellDictionary[newCell.id] = newCell;
+        board.appendChild( newNode );
+    
     }
 }
 
 function flag(id) {
-    
     if (document.getElementById(id).className == 'flag') {
         document.getElementById(id).className = 'cell';
     } else {
         document.getElementById(id).className = 'flag';
-    }
-}
-
-// Create an object storing the cell's class (mine or safe) and state (flagged or unflagged).
-// Add object to array at same 
-
-
-function createObject(cellId, gridArray) {
-    // placeholder for object logic if this is what I need to do...just barfing something out to work on later.
-    var cellGrid = {
-        id: cellId,
-        isMine: false,
-        isFlagged: false
     }
 }
