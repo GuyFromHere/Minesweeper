9a25bf2343ae8ce8210566851385d66c9f4645f7
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2019-05-27 02:47:45 -0700

    Board classes implemented, just need to add essential minesweeper logic

--- public/javascript.js
+++ public/javascript.js
@@ -1,16 +1,32 @@
 function getBoard(difficulty) {
-    // get board status and reload. 
-    // Has to be AJAX so it can store persistent board status. I'll need to learn more about that...
-    // For now I'm just working out the look and feel of the board.
-    if (!difficulty) {
-        var difficulty = 0;
-    }
+    // Add button to reset page with desired difficulty. 
+    // Load easy by default.
+    
+    // Variables
     let cellIndex = [ 4, 10, 16 ];
     let rowIndex = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
     var min = 1;
     var max = 10;
 
+    // reload empty board container div each time page refreshes
     const board = document.getElementById("board");
+    board.innerHTML = "";
+    board.className = "board";
+
+    // set board class based on selected difficulty
+    if (!difficulty) {
+        var difficulty = 0;
+        document.getElementById('board').classList.add('easy');
+    } else {
+        if (difficulty == 0) {
+            document.getElementById('board').classList.add('easy');
+        } else if (difficulty == 1) {
+            document.getElementById('board').classList.add('moderate');
+        } else if (difficulty == 2) {
+            document.getElementById('board').classList.add('hard');
+        }
+    }
+    
     // build board with area cellCount x cellCount
     for (var i = 0; i<cellIndex[difficulty]; i++) {  
         for (var x = 1; x<=cellIndex[difficulty]; x++) {
@@ -18,15 +34,17 @@ function getBoard(difficulty) {
             var random = Math.floor(Math.random() * (+max - +min)) + +min
 
             // create cell divs
-            var cellId = (rowIndex[i] + String(x));
+            const cellId = (rowIndex[i] + String(x));
             var newNode = document.createElement('div')
-            //newNode.setAttribute('id', cellId); 
             newNode.id = rowIndex[i] + x
-            //newNode.setAttribute('class', 'cell');
             newNode.className = 'cell';
-            //newNode.oncontextmenu = "javascript:alert('hello');return false;";
             newNode.setAttribute('oncontextmenu',"javascript:flag(this.id);return false;");
             newNode.count = i;
+            var newCell = {
+                id: newNode.id,
+                isMine: false,
+                isFlagged: false
+            };
 
             // define click behavior.
             if ( random > 5 ) {
@@ -34,6 +52,7 @@ function getBoard(difficulty) {
                     var element = document.getElementById(this.id);
                     element.className = 'boom';
                 };
+                newCell.isMine = true;
             } else {
                 newNode.onclick = function() {
                     var element = document.getElementById(this.id);
@@ -49,7 +68,7 @@ function getBoard(difficulty) {
 }
 
 function flag(id) {
-
+    
     if (document.getElementById(id).className == 'flag') {
         document.getElementById(id).className = 'cell';
     } else {
@@ -57,10 +76,15 @@ function flag(id) {
     }
 }
 
-function boom() {
-    alert('You died!');
-}
+// Create an object storing the cell's class (mine or safe) and state (flagged or unflagged).
+// Add object to array at same 
 
-function flip() {
-    alert('you safe!');
+
+function createObject(cellId, gridArray) {
+    // placeholder for object logic if this is what I need to do...just barfing something out to work on later.
+    var cellGrid = {
+        id: cellId,
+        isMine: false,
+        isFlagged: false
+    }
 }
