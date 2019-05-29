5c9842106f86f5b223f01e54079f64545d6e9f54
DismuckerDad <47902660+DismuckerDad@users.noreply.github.com>
2019-05-28 17:01:14 -0700

    Getting closer a working recursive reveal.

--- public/javascript.js
+++ public/javascript.js
@@ -7,6 +7,24 @@ const max = 10;
 // Empty dictionary to store cell objects.
 var cellDictionary = {};
 
+// Keep track of mine locations...for reasons? May not need it beyond troubleshooting.
+var mineArray = [];
+
+function resetLog() {
+    var logDiv = document.getElementById("log");
+    logDiv.innerHTML = "";
+}
+
+function addLog(text) {
+    var logDiv = document.getElementById("log");
+    logDiv.innerHTML += ('<p>' + text + '</p>');
+}
+
+function getRandomNumber(x, y) {
+    var random = Math.floor(Math.random() * (x * y));
+    return random;
+}
+
 function flag(id) {
     if (document.getElementById(id).className == 'flag') {
         document.getElementById(id).className = 'cell';
@@ -15,18 +33,75 @@ function flag(id) {
     }
 }
 
-function recursiveCellChecker(id, difficulty) {
-    // get list of neighbor cells from cellDictionary
-    // loop through each cell...
-    // if it's a mine, add to counter
-    // if not, move to next cell
-    // when finished, if counter > 0 make that the innerHTML
-    // if counter = 0 run this function against all the neighbor cells and clear the div HTML
+function clear(id) {
+    var element = document.getElementById(id);
+    element.className = 'clear';
+    element.innerHTML = "";
+}
+
+function safe(id) {
+    var element = document.getElementById(id);
+    element.className = 'safe'; 
+    element.innerHTML = cellDictionary[id].count;
+}
+
+/* function getNeighbors(id) {
+    // Function to get neighbor cell array and store it in object's neighbors property
+    // checks for boundaries of surrounding grid using object's difficulty property and cellIndex
+    // this way we'll have define the borders of the cell checker in the object itself
+    var cell = cellDictionary[id];
+} */
+
+// called on left click. This function sets the innerHTML to display the counter property of the cell.
+// if counter = 0, display an empty cell and get the counter properties of neighboring cells
+function cellChecker(id) {
+    var element = document.getElementById(id);
+    if ( cellDictionary[id].count == 0 ) {
+        clear(id);
+        whereTheMinesAt(id);
+
+    } else {
+        // not zero. change class to safe and display counter in innerHTML
+        safe(id);
+    }
+
+}
+
+// Add mines randomly to the board.
+function seedMines(Board) {
+    var mineCounter = 0;
+    do {
+        var randomCell = getRandomNumber(Board.x, Board.y); 
+        if ( !cellDictionary[randomCell].isMine ) {
+            cellDictionary[randomCell].isMine = true;
+            mineArray.push(randomCell);
+            mineCounter++;
+        }
+    }
+    while (mineCounter <= Board.mines );
+}
+
+//
+function whereTheMinesAt(id) {
+    var cell = cellDictionary[id];
+    addLog(cell.neighbors);
+    for ( var n of cell.neighbors )
+    {
+        addLog(cellDictionary[n].count);
+        if (cellDictionary[n].count == 0) {
+            clear(n);
+        } else {
+            safe(n);
+        }
+    }
+}
+
+// get surrounding mine count and store that as a property of the cell object
+function getSurroundingMines(id, difficulty) {
     let counter = 0;
-    //alert('check id = ' + id);
     for ( let neighborCell of cellDictionary[id].neighbors ) {
         if ( neighborCell < 0 || neighborCell >= (cellIndex[difficulty]*cellIndex[difficulty]) ) {
-            //do nothing
+            // Cells above or below the grid. Do nothing
         } else if ( Number(id) % cellIndex[difficulty] == 0) {
             // First column. Skip cells to the left.
             if ( neighborCell == ((Number(id)-cellIndex[difficulty])-1) ||
@@ -49,23 +124,18 @@ function recursiveCellChecker(id, difficulty) {
             counter += 1;
         }
     }
-    var element = document.getElementById(id);
-    element.className = 'safe'; 
-    element.innerHTML = counter; 
-    // 
-    if (counter == 0) {
-        for ( var all of cellDictionary[id].neighbors ) {
-            //alert('send to function: ' + cellDictionary[all].id)
-            recursiveCellChecker(cellDictionary[all].id, difficulty)
-        }
-    }
+    cellDictionary[id].count = counter;
 }
 
+
+
 function buildBoard(difficulty) {
+    resetLog();
     // reload empty board container div each time page refreshes (is there a better way to do this...???)
     const board = document.getElementById("board");
     board.innerHTML = "";
     board.className = "board";
+    addLog('Generating new board...');
 
     // set board class based on selected difficulty
     if (!difficulty || difficulty == 0) {
@@ -76,46 +146,48 @@ function buildBoard(difficulty) {
     } else if (difficulty == 2) {
         document.getElementById('board').classList.add('hard');
     }
-    let mineCounter = 0;
+
+    // Create board object to store board properties
+    let Board = {
+        x: cellIndex[difficulty],
+        y: cellIndex[difficulty],
+        mines: mineCount[difficulty]
+    }; 
 
     // grid boundaries (in relation to clicked cell) when checking for mines
-    const surroundingGrid = [ -(cellIndex[difficulty]-1), -(cellIndex[difficulty]), -(cellIndex[difficulty]+1), 
+    const surroundingGrid = [ -(cellIndex[difficulty]+1), -(cellIndex[difficulty]), -(cellIndex[difficulty]-1), 
         -1, 1, (cellIndex[difficulty]-1), (cellIndex[difficulty]), (cellIndex[difficulty]+1) ];
 
     // loop through board dimensions seeding with mines
     for (var i = 0; i<(cellIndex[difficulty]*cellIndex[difficulty]); i++) {  
-        // get random number to determine if mine or safe
-        var random = Math.floor(Math.random() * (+max - +min)) + +min
-        
         // define div 
-        var newNode = document.createElement('div')
+        var newNode = document.createElement('div');
         newNode.id = i;
         newNode.className = 'cell';
+        newNode.innerHTML = i;
         newNode.setAttribute('oncontextmenu',"javascript:flag(this.id);return false;");
         newNode.setAttribute('onclick','javascript:sweep(this.id);');
 
         // get neighbor cells based on div id
         let neighborCells = [];
-        // TODO: Get rid of the nested loop if possible. 
         for (var cells of surroundingGrid) {
             let neighborCell = Number(i) + Number(cells);
             neighborCells.push(neighborCell);
         }
         let newCell = {
             id: newNode.id,
+            difficulty: difficulty,
             neighbors: neighborCells
         }; 
 
-        // TODO: Find a better method of ensuring the mines are randomly dispersed in the grid.
-        if ( random > 8 && mineCounter <= mineCount[difficulty] ) {
-            newCell.isMine = true;
-            mineCounter += 1;
-        } else {
-            newCell.isMine = false;
-        }
-        // 
         cellDictionary[newCell.id] = newCell;
         board.appendChild( newNode );
+    }
+    // seed mines 
+    seedMines(Board);
+    // now that we know where the mines are we can count them and store that as a cell property
+    for (var i = 0; i<(cellIndex[difficulty]*cellIndex[difficulty]); i++) {  
+        getSurroundingMines(i, difficulty);
     }
 }
 
@@ -131,6 +203,7 @@ function buildBoard(difficulty) {
 
 
 
+// OLD BELOW
 
 function getBoard(difficulty) {
     // reload empty board container div each time page refreshes (is there a better way to do this...???)
---
+++ public/javascript1.js
@@ -0,0 +1,11 @@
+/*
+    1. Draw board using fixed size (work on size options later)
+    2. Each cell is an object that stores: id, neighbors, 
+*/
+
+var Board = ne
+
+function drawBoard()
+{
+
+}
